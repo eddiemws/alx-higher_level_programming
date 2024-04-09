6471eb21cbc78e1f36032b6bc937e9bbacda03a2
eddy <106661971+eddiemws@users.noreply.github.com>
2024-04-09 19:17:51 +0300

    Update 101-locked_class.py

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,5 +1,5 @@
 class LockedClass:
     def __setattr__(self, name, value):
-        if name != 'first_name':
-            raise AttributeError("Cannot dynamically create new instance attributes")
+        if not hasattr(self, 'first_name') and name != 'first_name':
+            raise AttributeError("'LockedClass' object has no attribute '{}'".format(name))
         super().__setattr__(name, value)
