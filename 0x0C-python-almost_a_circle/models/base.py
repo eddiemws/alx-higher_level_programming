3f665bde1165ea39dc2bb3dab321a58db37311fe
eddy <106661971+eddiemws@users.noreply.github.com>
2024-04-19 21:46:52 +0300

    Update base.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -4,8 +4,25 @@ import json
 import csv
 import turtle
 
+class Base():
+    '''Represent a base model.
 
+    Attribute:
+       __nb_objects (): The number of instantiated bases.
+    '''
+    __nb_objects = 0
 
+    def __init__(self, id=None):
+        '''Initialize a new base.
+
+        Args:
+           id (int): The identity of a new base.
+        '''
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
 
     def from_json_string(json_string):
         '''Returns the list of the JSON string representation json_string.
