fffc9c2bed92199a0bcdae554aba69d977bf61f9
eddy <106661971+eddiemws@users.noreply.github.com>
2024-06-27 00:30:03 +0300

    Update 102-relationship_cities_states_list.py

--- 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -1,22 +1,38 @@
 #!/usr/bin/python3
 """
-Lists all City objects from the database hbtn_0e_101_usa
+Lists all City objects from the database hbtn_0e_101_usa.
+Usage: ./102-relationship_cities_states_list.py <mysql username> <mysql password> <database name>
 """
-import sys
-from relationship_state import Base, State
-from relationship_city import City
+
+from sys import argv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from relationship_state import State
+from relationship_city import City
 
+if __name__ == "__main__":
+    if len(argv) != 4:
+        print("Usage: ./102-relationship_cities_states_list.py <mysql username> <mysql password> <database name>")
+        exit(1)
 
-if __name__ == '__main__':
-engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]),pool_pre_ping=True)
+    username = argv[1]
+    password = argv[2]
+    db_name = argv[3]
 
-Session = sessionmaker(bind=engine)
-session = Session()
+    # Create an engine and connect to the MySQL server
+    engine = create_engine(f'mysql+mysqldb://{username}:{password}@localhost:3306/{db_name}')
 
-st = session.query(State).join(City).order_by(City.id).all()
+    # Create a configured "Session" class
+    Session = sessionmaker(bind=engine)
+    # Create a Session
+    session = Session()
 
-for state in st:
-for city in state.cities:
-print("{}: {} -> {}".format(city.id, city.name, state.name))
+    # Query all City objects, sorted by cities.id
+    cities = session.query(City).order_by(City.id).all()
+
+    # Print results
+    for city in cities:
+        print(f"{city.id}: {city.name} -> {city.state.name}")
+
+    # Close session
+    session.close()
