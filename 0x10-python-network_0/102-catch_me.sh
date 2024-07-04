8339f10f41c5d2910a866b81a1f5a667a69e05a9
eddy <106661971+eddiemws@users.noreply.github.com>
2024-07-04 09:57:54 +0300

    Create 102-catch_me.sh

---
+++ 0x10-python-network_0/102-catch_me.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Makes a request to 0.0.0.0:5000/catch_me to get a response containing "You got me!" in the body
+
+curl -sL -X PUT -d "user_id=98" -H "Origin: HolbertonSchool" 0.0.0.0:5000/catch_me
